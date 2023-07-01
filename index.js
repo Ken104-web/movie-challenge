66e355cb2615c6dac5361a93317a15207ba75516
KenCorp <kennethwarui35@gmail.com>
2023-07-01 10:52:12 +0300

    Test done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Waiting for the DOM to load
 document.addEventListener('DOMContentLoaded', function(){
+    displayMenu();
     movieDetails();
 });
 function movieDetails(){
@@ -7,7 +8,7 @@ return fetch('http://localhost:3000/films')// Makes a GET request
  .then((resp) => resp.json())// Converts the responce
  .then((json) => displayMovies(json));// Passes the movie date to displayMovies 
 }
-// Show a list of films on the webpage
+// Show the details of the first on the webpage
 function displayMovies(movies){
  const div = document.querySelector('div');
  div.innerHTML = ''; //clear the div element before rendering
@@ -26,4 +27,34 @@ function displayMovies(movies){
  </div>
  `;
  div.innerHTML = movieDetailsHTML;
-};
+ displayMenu()
+}
+function displayMenu(){
+    // Make a GET request
+ fetch('http://localhost:3000/films')
+  .then((resp) => resp.json())// Convert the resp to a javascript
+  .then((json) => movieList(json));// pass the data to movieList
+}
+// show a list of movies on the webpage
+function movieList(films) {
+    let showListHTML = '';
+    films.forEach((film) => {
+      showListHTML += `<li class="film-item">${film.title}</li>`;
+    });
+  
+    const filmsList = document.getElementById('films');
+    if (!filmsList) {
+      // Create the <ul> element if it doesn't exist
+      const ul = document.createElement('ul');
+      ul.id = 'films';
+      document.body.appendChild(ul); 
+      filmsList = ul;
+    }
+  
+    filmsList.innerHTML = ''; // Clear the existing content
+    filmsList.insertAdjacentHTML('beforeend', showListHTML);
+  }
+  
+ 
+
+
