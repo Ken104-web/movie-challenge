c9dc524299db04089b923eeda8e1b9dc83560626
KenCorp <kennethwarui35@gmail.com>
2023-07-02 11:01:38 +0300

    Test done

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Waiting for the DOM to load
 document.addEventListener('DOMContentLoaded', function(){
-    displayMenu();
-    movieDetails();
+    displayMenu(); // Display the movie list
+    movieDetails(); // Display details of the first movie
 });
+
+// Fetch movie data and display movie details
 function movieDetails() {
-    return fetch('http://localhost:3000/films') // Makes a GET request
-        .then((resp) => resp.json()) // Converts the response
-        .then((json) => displayMovies(json)); // Passes the movie data to displayMovies
+    return fetch('http://localhost:3000/films') // Makes a GET request to fetch movie data
+        .then((resp) => resp.json()) // Converts the response to JSON format
+        .then((json) => displayMovies(json)); // Passes the movie data to displayMovies function
 }
-// Show the details of the first movie on the webpage
+
+// Display the details of the first movie on the webpage
 function displayMovies(movies) {
     const div = document.querySelector('div');
     div.innerHTML = '';
+
     // Displaying the first movie details
     let firstMovie = movies[0];
-    if (!firstMovie) {
-        // Handle case where there are no movies
-        div.innerHTML = 'No movies available';
-        return;
-    }
+
     let { poster, title, runtime, showtime, tickets_sold } = firstMovie;
     let fullCapacity = 150;
 
@@ -42,19 +42,22 @@ function displayMovies(movies) {
     const filmItems = document.querySelectorAll('.film-item');
     filmItems.forEach((filmItem, index) => {
         filmItem.addEventListener('click', () => {
-            const filmId = movies[index].id; // Get the movie ID from the movies array
-            fetch(`http://localhost:3000/films/${filmId}`) // Make a GET request for the specific movie
-                .then((resp) => resp.json()) // Convert the response to JavaScript
-                .then((json) => displayMovies([json])); // Pass the movie data to displayMovies
+            if (movies[index] && movies[index].id) {
+                const filmId = movies[index].id; // Get the movie ID from the movies array
+                fetch(`http://localhost:3000/films/${filmId}`) // Make a GET request for the specific movie
+                    .then((resp) => resp.json()) // Convert the response to JavaScript
+                    .then((json) => displayMovies([json])); // Pass the movie data to displayMovies
+            }
         });
     });
 }
-// fetching 
+
+// Fetch movie data and display movie list
 function displayMenu() {
-    // Make a GET request
+
     fetch('http://localhost:3000/films')
-        .then((resp) => resp.json()) // Convert the response to JavaScript
-        .then((json) => movieList(json)); // Pass the data to movieList
+        .then((resp) => resp.json())
+        .then((json) => movieList(json));
 }
 
 // Show a list of movies on the webpage
@@ -65,18 +68,11 @@ function movieList(films) {
     });
 
     let filmsList = document.getElementById('films');
-    if (!filmsList) {
-        // Create the <ul> element if it doesn't exist
-        const ul = document.createElement('ul');
-        ul.id = 'films';
-        document.body.appendChild(ul);
-        filmsList = ul;
-    }
-
     filmsList.innerHTML = ''; // Clear the existing content
     filmsList.insertAdjacentHTML('beforeend', showListHTML);
 }
 
+// Buy a movie ticket
 function buyTicket(movie) {
     let { tickets_sold } = movie;
     let fullCapacity = 150;
